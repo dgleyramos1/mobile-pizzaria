0ca97b240e3715c3490415532bb6282e01e4ac0f
Dgley Ramos <dgleysilva@gmail.com>
2022-12-19 19:42:19 -0300

    Removendo itens da lista

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -131,6 +131,19 @@ export default function Order(){
         setItems(oldArray => [...oldArray, data])
     }
 
+    async function handleDeleteItem(item_id: string){
+        await api.delete('/order/remove', {
+            params: {
+                item_id: item_id
+            }
+        })
+        let removeItem = items.filter(item => {
+            return (item.id !== item_id)
+        })
+
+        setItems(removeItem)
+    }
+
     return(
         <View style={styles.container}>
             <View style={styles.header}>
@@ -198,7 +211,7 @@ export default function Order(){
                 style={{flex: 1, marginTop: 24}}
                 data={items}
                 keyExtractor={(item) => item.id}
-                renderItem={({item}) => <LIstItem data={item}/>}
+                renderItem={({item}) => <LIstItem data={item} deleteItem={handleDeleteItem}/>}
             />
 
             <Modal
