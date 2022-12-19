308a566123c405431a723ef2eb0084a5f4cc2725
Dgley Ramos <dgleysilva@gmail.com>
2022-12-19 05:07:41 -0300

    Abrindo mesa

---
+++ src/pages/Order/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View, StyleSheet } from "react-native";
+
+
+export default function Order(){
+    return(
+        <View style={styles.container}>
+            <Text>Tela Order</Text>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
