dba109ccd5fc00c59c1676c08e93f8d326c17379
Dgley Ramos <dgleysilva@gmail.com>
2022-12-19 19:28:27 -0300

    Adicionando item ao pedido

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,6 +1,7 @@
 
 
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import {Feather} from '@expo/vector-icons'
 
 interface ItemProps{
     data: {
@@ -14,7 +15,10 @@ interface ItemProps{
 export function LIstItem({data}: ItemProps){
     return(
         <View style={styles.container}>
-            <Text>Item da lista</Text>
+            <Text style={styles.item}>{data.amount} - {data.name}</Text>
+            <TouchableOpacity>
+                <Feather name="trash-2" color="#ff3f4b" size={25}/>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -27,6 +31,19 @@ export function LIstItem({data}: ItemProps){
 
 const styles = StyleSheet.create({
     container: {
-
+        flex: 1,
+        backgroundColor: '#101026',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        marginBottom: 12,
+        paddingVertical: 12,
+        paddingHorizontal: 12,
+        borderRadius: 4,
+        borderWidth: 0.3,
+        borderColor: '#8a8a8a'
+    }, 
+    item: {
+        color: '#fff'
     }
 })
--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -113,14 +113,36 @@ export default function Order(){
         setProductSelected(item)
     }
 
+    async function handleAdd(){
+        const response = await api.post('/order/add', {
+            order_id: route.params?.order_id,
+            product_id: productSeleted?.id,
+            amount: Number(amount)
+        })
+
+
+        let data = {
+            id: response.data.id,
+            product_id: productSeleted?.id as string,
+            name: productSeleted?.name as string,
+            amount: amount
+        }
+
+        setItems(oldArray => [...oldArray, data])
+    }
+
     return(
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.title}>Mesa {route.params.number}</Text>
 
-                <TouchableOpacity onPress={handleCloseOrder}>
-                    <Feather name="trash-2" size={28} color="#ff3f4b"/>
-                </TouchableOpacity>
+                {
+                    items.length === 0 && (
+                        <TouchableOpacity onPress={handleCloseOrder}>
+                            <Feather name="trash-2" size={28} color="#ff3f4b"/>
+                        </TouchableOpacity>
+                    )   
+                }
             </View>
 
             {
@@ -159,7 +181,7 @@ export default function Order(){
 
 
             <View style={styles.actions}>
-                <TouchableOpacity style={styles.buttonAdd}>
+                <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
                     <Text style={styles.buttonText}>+</Text>
                 </TouchableOpacity>
 
