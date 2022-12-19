2acb73ef23b0c8cc005b98f7c4927e1def79e874
Dgley Ramos <dgleysilva@gmail.com>
2022-12-19 03:55:46 -0300

    Permanecendo o user logado

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,11 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useState, createContext, ReactNode} from "react";
+import { useState, createContext, ReactNode, useEffect} from "react";
 import { api } from "../services/api";
 
 type AuteContentData = {
     user: UserProps;
     isAuthenticated: boolean;
     signIn: (credential: SignInProps) => Promise<void>
+    loadingAuth: boolean
+    loading: boolean
+    signOut: () => Promise<void>
 }
 
 type UserProps = {
@@ -37,8 +40,35 @@ export function AuthProvider({children}: AuthProviderProps){
     })
 
     const [loadingAuth, setLoadingAuth] = useState(false)
+    const [loading, setLoading] = useState(true)
 
     const isAuthenticated = !!user.name;
+
+    useEffect(() => {
+        async function getUser(){
+
+            // pegar os dados salvos do user
+            const userInfo = await AsyncStorage.getItem('@sujeitopizzaria');
+            let hasUser: UserProps = JSON.parse(userInfo || '{}');
+
+
+            // verificar se recebemos as informações dele
+            if(Object.keys(hasUser).length > 0){
+                api.defaults.headers.common['Authorization'] = `Bearer ${hasUser.token}`
+
+                setUser({
+                    id: hasUser.id,
+                    name: hasUser.name,
+                    email: hasUser.email,
+                    token: hasUser.token,
+                })
+            }
+
+            setLoading(false)
+        }
+
+        getUser()
+    })
 
     async function signIn({email, password}: SignInProps){
         setLoadingAuth(true)
@@ -73,8 +103,29 @@ export function AuthProvider({children}: AuthProviderProps){
         }
     }
 
+    async function signOut(){
+        await AsyncStorage.clear()
+        .then( () => {
+            setUser({
+                id: '',
+                name: '',
+                email: '',
+                token: ''
+            })
+        })
+    }
+
     return (
-        <AuthContext.Provider value={{user, isAuthenticated, signIn}}>
+        <AuthContext.Provider
+            value={{
+                user,
+                isAuthenticated,
+                signIn,
+                loadingAuth,
+                loading,
+                signOut
+            }}
+        >
             {children}
         </AuthContext.Provider>
     )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,9 +11,8 @@ import AuthRoutes from "./auth.routes";
 
 function Routes(){
 
-    const {isAuthenticated} = useContext(AuthContext);
-    
-    const loading = false;
+    const {isAuthenticated, loading} = useContext(AuthContext);
+
 
     if(loading){
         return(
